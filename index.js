b5e7bba0f4b3cb00d705662009a8d6ac3e8cb266
MiyoshiNakajima <a5821066@aoyama.jp>
2022-06-17 17:50:41 +0900

    student3_2

--- index.js
+++ index.js
@@ -70,9 +70,9 @@ function onTaskTitleClicked(task) {
     console.log("タイトルがクリックされました。", task);
 
     // 達成状態を更新(このままだと、現在の達成状態で更新されてしまう！)
-    const currentValue = task.isDone;
+    task= !task.isDone;
     
-    task.isDone = currentValue;
+
 
     return task;
 }
